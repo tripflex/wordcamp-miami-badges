505a07ccf15c89d3957da5e5562b2f1656694259
FX Bénard <fxbenard@gmail.com>
2014-04-30 11:57:19 +0200

    Bad image link correction

--- includes/element-wordcamp_miami_badge.php
+++ includes/element-wordcamp_miami_badge.php
@@ -10,9 +10,9 @@
 	<?php
 		if($badge_type == 'attending'):
 	?>
-			<a href="http://2014.miami.wordcamp.org/"><img src="http://2014.miami.wordcamp.org/files/2014/03/wcmia_2014_badge_sponsoring.png" alt="I am a Proud Sponsor of WordCamp Miami 2014" title="I am a Proud Sponsor of WordCamp Miami 2014" width="150" height="175" /></a>
-	<?php 
-		elseif($badge_type == 'speaking'): 
+			<a href="http://2014.miami.wordcamp.org/"><img src="http://2014.miami.wordcamp.org/files/2014/03/wcmia_2014_badge_attending.png" alt="I am Attending WordCamp Miami 2014" title="I am Attending WordCamp Miami 2014" width="150" height="175" /></a>
+	<?php
+		elseif($badge_type == 'speaking'):
 	?>
 			<a href="http://2014.miami.wordcamp.org/"><img src="http://2014.miami.wordcamp.org/files/2014/03/wcmia_2014_badge_speaking.png" alt="I am Speaking at WordCamp Miami 2014" title="I am Speaking at WordCamp Miami 2014" width="150" height="175" /></a>
 	<?php
