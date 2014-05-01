1dc715af99bf97063da766f5cfc870632b5072c5
Myles McNamara <myles@smyl.es>
2014-05-01 17:22:29 -0400

    change back to /languages

--- plugincore.php
+++ plugincore.php
@@ -18,7 +18,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: tripflex/wordcamp-miami-badges
  * GitHub Branch:   master
- * Domain Path: /lang
+ * Domain Path: /languages
  */
 
 // If this file is called directly, abort.
